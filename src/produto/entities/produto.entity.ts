c38b217f86fa75e62fb0389bf1363033da25a1e3
pe3dru <lionpedro30@gmail.com>
2025-10-07 14:45:15 -0300

    CRUD_Categoria

--- src/produto/entities/produto.entity.ts
+++ src/produto/entities/produto.entity.ts
@@ -1,5 +1,6 @@
 import { IsNotEmpty } from 'class-validator';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Categoria } from '../../categoria/entities/categoria.entity';
 
 @Entity({ name: 'tb_produtos' })
 export class Produto {
@@ -17,4 +18,9 @@ export class Produto {
   @IsNotEmpty()
   @Column({ type: 'decimal', precision: 10, scale: 2 })
   preco: number;
+
+  @ManyToOne(() => Categoria, (categoria) => categoria.produto, {
+    onDelete: 'CASCADE',
+  })
+  categoria: Categoria;
 }
